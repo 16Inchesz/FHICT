f2ae8caabecde6ff4186bcccd5a8c065082ca9a3
John <j.domacasse@student.fontys.nl>
2023-03-29 19:13:57 +0200

    changes to pointer arithmetic

--- CODES/Open Space (C) - SEM 2/Assignment week 7/exercise 4/src/main.c
+++ CODES/Open Space (C) - SEM 2/Assignment week 7/exercise 4/src/main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
 bool Search_value(int *a, int size_array, int value, bool* result){
     if(result == NULL || a == NULL){
         return false;
     }
     for (int i = 0; i < size_array; i++){
-        if (a[i] == value){
+        if (*a == value){
             *result = true;
             return true;
         }
+        a++;
     }
     *result = false;
     return true;
@@ -20,26 +28,22 @@ bool calculate_occurences(int *a, int size_array, int value, int* occurrence){
         return false;
     }
     for (int i = 0; i < size_array; i++){
-        if (a[i] == value){
+        if (*a == value){
             *occurrence += 1;
-        }    
+        } 
+        a++;   
     }
     return true;
 }
 
-void swap(int* x, int* y)
-{
-	int temp = *x;
-	*x = *y;
-	*y = temp;
-}
 
 bool reverse_order(int *a, int size_array){
     if(a == NULL){
         return false;
     }
     for (int i = 0; i < size_array / 2; i++){
-        swap(&a[i], &a[size_array - i - 1]);
+        //swap(*a, *(a+size_array - i - 1))
+        swap(&a[i], &a[size_array - i - 1]);    
     }
     return true;
 }
@@ -53,6 +57,9 @@ bool bubble_sort(int *a, int size_array){
             if (a[j] > a[j+1]){
                 swap(&a[j], &a[j+1]);
             }
+            // if (*a > *(a + 1)){
+            //  swap(*a, *(a + 1))
+            // }
         }
     }
     return true;
@@ -67,28 +74,28 @@ int main()
     int occurence = 0;
     int size_array = sizeof(a)/sizeof(a[0]);
 
-    // if (Search_value(a, size_array, value, &result)){
-    //     if(result){
-    //         printf("the value is in the array\n");
-    //     } else {
-    //         printf("this value is not in the array\n");
-    //     }
-    // } else {
-    //     printf("pointer not provided\n");
-    // }
+    if (Search_value(a, size_array, value, &result)){
+        if(result){
+            printf("the value is in the array\n");
+        } else {
+            printf("this value is not in the array\n");
+        }
+    } else {
+        printf("pointer not provided\n");
+    }
 
-    // if (calculate_occurences(a, size_array, value, &occurence)){
-    //     printf("this value occurs %d times\n", occurence);
-    // } else {
-    //     printf("pointer not provided");
-    // }
+    if (calculate_occurences(a, size_array, value, &occurence)){
+        printf("this value occurs %d times\n", occurence);
+    } else {
+        printf("pointer not provided");
+    }
 
-    // if (reverse_order(a, size_array)){
-    //     for (int i = 0; i < size_array; i++){
-    //         printf("%d", a[i]);
-    //     }
-    //     printf("\n");
-    // }
+    if (reverse_order(a, size_array)){
+        for (int i = 0; i < size_array; i++){
+            printf("%d ", a[i]);
+        }
+        printf("\n");
+    }
 
     if (bubble_sort(a, size_array)){
         for (int i = 0; i < size_array; i++){
