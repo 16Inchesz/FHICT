7cc4ebd178f43d0e535e67ba2cc25681f96dfaa9
John <j.domacasse@student.fontys.nl>
2023-05-28 21:17:01 +0200

    work on encoder tomorrow

--- CODES/Open Space C - SEM 2/C Assignment 3 - part 1/Encoder/src/encoder.c
+++ CODES/Open Space C - SEM 2/C Assignment 3 - part 1/Encoder/src/encoder.c
@@ -2,4 +2,10 @@
 
 bool encodeByte(uint8_t *inputByte, uint8_t *encodedByteHigh, uint8_t *encodedByteLow){
     
+    //NULL input pointers
+    if (inputByte == NULL || encodedByteHigh == NULL || encodedByteLow == NULL){
+        return false;
+    }
+
+    
 }
