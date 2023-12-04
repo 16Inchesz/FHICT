7d9137f9763149ce95167442ab26cc4770271c38
Johnson Domcasse <j.domacasse@student.fontys.nl>
2023-12-04 12:41:25 +0100

    Tree done

--- SEM3/System Design/Assignment 6/ass6/1/ass6_1.cpp
+++ SEM3/System Design/Assignment 6/ass6/1/ass6_1.cpp
@@ -2,18 +2,61 @@
 
 using namespace std;
 
+//references: mycodeschool, geeksofrgeeks, code standard
+struct NODE{
+	int data;
+	NODE* leftChild;
+	NODE* rightChild;
+};
+
+//DFS algorithm: geeksforgeeks
+int DFS_Algortihm(NODE* node){
+	if (node == nullptr){
+		return 0;
+	}
+	else {
+		int leftDepth = DFS_Algortihm(node->leftChild);
+		int rightDepth = DFS_Algortihm(node->rightChild);
+		
+		if (leftDepth > rightDepth)
+			return (leftDepth + 1);
+		else
+			return (rightDepth + 1);
+	}
+}
+
+//function to build the tree based on the input file
+NODE* buildTree(int n) {
+    NODE* nodes = new NODE[n + 1];	//0 also counts as our nullptr in the case.
+    NODE* root = nullptr;
+
+    for (int i = 0; i < n; i++) {
+        int nr, left, right;
+        cin >> nr >> left >> right;
+
+        nodes[nr].data = nr;
+
+        if (left != 0)
+            nodes[nr].leftChild = &nodes[left];
+
+        if (right != 0)
+            nodes[nr].rightChild = &nodes[right];
+
+        if (i == 0)
+            root = &nodes[nr];
+    }
+	
+    return root;
+}
+
 int main()
 {
 	int n;
 	cin >> n;
 
-	for(int i=0;i<n;i++)
-	{
-		int nr, left, right;
-		cin >> nr >> left >> right;
-	}
-	int depth = 0;
-	/* implement your algorithm and store the tree depth in depth */
+	NODE* root = buildTree(n);
+
+	int depth = DFS_Algortihm(root);
 	cout << "depth is " << depth << endl;
 	return 0;
 }
